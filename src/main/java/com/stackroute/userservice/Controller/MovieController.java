3be87c6082a1f04677563825e61c30f0ab5c6428
haripriya.gummadi <haripriya.gummadi@cgi.com>
2019-10-03 15:32:14 +0530

    used swagger

--- src/main/java/com/stackroute/userservice/Controller/MovieController.java
+++ src/main/java/com/stackroute/userservice/Controller/MovieController.java
@@ -1,14 +1,19 @@
 package com.stackroute.userservice.Controller;
 import com.stackroute.userservice.Model.Movie;
 import com.stackroute.userservice.Service.MovieService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
+import java.util.Map;
 
+@Api(value="Movie Management System", description="Operations pertaining to movie in Movie Management System")
 @RestController
 @RequestMapping("/userservice")
 public class MovieController {
@@ -17,10 +22,14 @@ public class MovieController {
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
+
+    @ApiOperation(value = "View a list of available movies", response = List.class)
     @GetMapping("movie")
     public ResponseEntity<?> getAllMoviesController(){
         return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
     }
+
+    @ApiOperation(value = "Add a movie")
     @PostMapping("movie")
     public ResponseEntity<?> saveMovieController(@RequestBody Movie movie){
 
@@ -33,6 +42,7 @@ public class MovieController {
         }
     }
 
+    @ApiOperation(value = "Update a movie")
     @PutMapping("movie")
     public ResponseEntity<?> updateMovieController(@RequestBody Movie movie){
         try {
@@ -42,8 +52,10 @@ public class MovieController {
             return new ResponseEntity<String>(ex.getMessage(),HttpStatus.CONFLICT);
         }
     }
-    @DeleteMapping("movie")
-    public ResponseEntity<?> deleteMovieController(@RequestBody long id){
+
+    @ApiOperation(value = "Delete a movie")
+    @DeleteMapping("movie/{id}")
+    public ResponseEntity<?> deleteMovieController(@PathVariable("id") long id){
         try {
             return new ResponseEntity<Boolean>(movieService.deleteMovie(id),HttpStatus.ACCEPTED);
         }
@@ -51,11 +63,10 @@ public class MovieController {
             return new ResponseEntity<String>(ex.getMessage(),HttpStatus.CONFLICT);
         }
     }
-    @GetMapping("movie/title")
-    public ResponseEntity<?> searchByNameController(@RequestBody String string){
+    @ApiOperation(value = "Get a movie by title")
+    @GetMapping("movie/{title}")
+    public ResponseEntity<?> searchByNameController(@PathVariable("title") String string){
         try {
-            System.out.println(string);
-            System.out.println("in controller");
             return new ResponseEntity<List<Movie>>(movieService.findByName(string),HttpStatus.OK);
         }
         catch (Exception ex){
--- src/main/java/com/stackroute/userservice/Model/Movie.java
+++ src/main/java/com/stackroute/userservice/Model/Movie.java
@@ -1,13 +1,21 @@
 package com.stackroute.userservice.Model;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
+@ApiModel(description = "All details about the Movies. ")
 @Entity
 public class Movie {
+
     @Id
+    @ApiModelProperty(notes = "id of the movie")
     private long id;
+    @ApiModelProperty(notes = "title of the movie")
     private String  original_title;
+    @ApiModelProperty(notes = "language of the movie")
     private String  original_language;
+    @ApiModelProperty(notes = "released date of the movie")
     private String  release_date;
 
     public Movie() {
