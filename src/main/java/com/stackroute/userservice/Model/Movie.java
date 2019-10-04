ef7f807ecebb03585f91a653874d0805f920cc3f
haripriya.gummadi <haripriya.gummadi@cgi.com>
2019-10-04 12:54:34 +0530

    implemented lombok

--- src/main/java/com/stackroute/userservice/Model/Movie.java
+++ src/main/java/com/stackroute/userservice/Model/Movie.java
@@ -1,9 +1,14 @@
 package com.stackroute.userservice.Model;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-
+import lombok.*;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @ApiModel(description = "All details about the Movies. ")
 @Entity
 public class Movie {
@@ -17,56 +22,4 @@ public class Movie {
     private String  original_language;
     @ApiModelProperty(notes = "released date of the movie")
     private String  release_date;
-
-    public Movie() {
-    }
-
-    public Movie(long id, String original_title, String original_language, String release_date) {
-        this.id = id;
-        this.original_title = original_title;
-        this.original_language = original_language;
-        this.release_date = release_date;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getOriginal_title() {
-        return original_title;
-    }
-
-    public void setOriginal_title(String original_title) {
-        this.original_title = original_title;
-    }
-
-    public String getOriginal_language() {
-        return original_language;
-    }
-
-    public void setOriginal_language(String original_language) {
-        this.original_language = original_language;
-    }
-
-    public String getRelease_date() {
-        return release_date;
-    }
-
-    public void setRelease_date(String release_date) {
-        this.release_date = release_date;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "id=" + id +
-                ", original_title='" + original_title + '\'' +
-                ", original_language='" + original_language + '\'' +
-                ", release_date='" + release_date + '\'' +
-                '}';
-    }
 }
